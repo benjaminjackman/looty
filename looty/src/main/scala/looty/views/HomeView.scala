68fe89d8ec97c41d50fad8fa3da6321adad0fd47
Ben Jackman <ben@jackman.biz>
2014-08-25 13:53:29 -0500

    Version history as it's own string in homeview (not used yet)
    Signed-off-by: Ben Jackman <ben@jackman.biz>

--- looty/src/main/scala/looty/views/HomeView.scala
+++ looty/src/main/scala/looty/views/HomeView.scala
@@ -9,103 +9,155 @@ import org.scalajs.jquery.JQuery
 //////////////////////////////////////////////////////////////
 
 
-class HomeView(val banner : String, val version : String) extends View {
+class HomeView(val banner: String, val version: String) extends View {
+  def versionHistory = """
+# Version History
+
+## 0.2.1.10
+* Rewrote Gem XP Tracker. Try it out and let me know what you guys think!
+* Using select2 menus on the datagrids for user defined views (saveable sets of columns / filters)
+* Added ACE / vm.js (for custom scores, not yet implemented)
+
+## 0.2.1.9
+* Numeric columns don't require operators (e.g. < <= etc) anymore. Those operators can still be used however they should filter as a min or max automatically. For example typing 50 in the pDps column will only show items with a pDps greater than or equal to 50. while typing it into the rLvl column (RequiredLevel) will show items that require at most level 50.
+* Strength and Int Bonuses are now applied to life / mana  (thanks to sundrino for the suggestion)
+* Increased cast speed, accuracy and max energy shield all have columns
+
+## 0.2.1.8 (2014-08-22)
+* shift click on a character / tab in [tabs] to refresh that character or tab from pathofexile.com
+* more descriptive types
+* larger column headers / smaller fonts
+
+## 0.2.1.7 (2014-08-22)
+* Show empty cells instead of zeros
+
+## 0.2.1.6 (2014-08-22)
+* Fixed sizing issue with PoeBuilder Page
+
+## 0.2.1.5 (2014-08-22)
+* Add / Remove / Save Columns + Filters
+* Add / Remove / Refresh Tabs
+* Open a characters build in PoeBuilder (Poe Builder is awesome check it out sometime!)
+* rarity column (thanks to sundrino for the suggestion)
+
+## 0.2.1.4 (2014-08-21)
+* Fixed bug with Block % not parsing correctly
+
+## 0.2.1.3 (2014-08-21)
+* Fixed bug with max level gems causing parse errors
+* Upgrade to ScalaJs 0.5.3
+
+## 0.2.1.2 (2014-08-20)
+* Fixed google analytics script errors
+
+## 0.2.1.1 (2014-08-20)
+* Added a misc column that shows count of items in stack / mapLevel / gemLevel / Total #Sockets  (thanks to hih0, Sirais, and tokyotapes for the suggestions)
+
+## 0.2.1 (2014-08-20)
+* Fixed DPS Bug (thanks to SoulProxy for reporting to the issue)
+
+## 0.2.0 (2014-08-19)
+* Chrome Web Store Release
+
+   """
+
+
   def start(el: JQuery) {
     el.html {
       """<div class="home">""" +
-      s"""<h1 style="color:red">$banner</h1>""" +
-      """<h2><img src="images/coin16.png">Welcome to <span style="color:gold">Looty!</span><img src="images/coin16.png">""" +
-      s"""</h2>Version: ($version)<p>""" +
-      """<span style="color:gold">Looty</span> was created to make it easier to search inventories in Path of Exile.<br>
-      |It provides a grid interface to search for items in different leagues.<br>
-      |The XP view will attempt to predict when a gem will level up.<br>
-      |GGG seems to only refresh items and gem information when a character changes zones,
-      |so keep that in mind if things aren't showing up.<br>
-      |source code available on <a href="https://github.com/benjaminjackman/looty">github</a>.<br>
-      |<h3>Help and more info available <a href="http://blog.jackman.biz/looty">here</a> (eventually)</h3>
-      |<h4> Tips & Tricks & Miscellaneous Details </h4>
-      |<ul>
-      |<li>You can type things to search for into the empty row at the top of the grids.
-      |<ul>
-      |<li>Try starting the search with &gt; or &lt; or =</li>
-      |<li>The search is not case sensitive.</li>
-      |<li>If the first term isn't an operator, then it defaults to regular expression search.</li>
-      |<li>Spaces between words are treated as an 'OR', so only one word has to match to return a result.</li>
-      |<li>If you need to search for a space just use a \s instead.</li>
-      |<li>For example brass maul, will match 'brass maul' and 'spiny maul' and 'brass monkey'</li>
-      |<li>Whereas brass\smaul will match only 'brass maul'.</li>
-      |</ul>
-      |</li>
-      |<li>You can type into multiple columns to filter down to what you want, for example to find boots with a red
-      |socket that can be worn by characters under level 35, enter <i>boots</i> in the <b>type</b> column
-      |and <i>r</i> in the <b>sockets</b> column and <i>&lt;35</i> in the <b>rlvl</b> column.
-      |</li>
-      |<li>Click the column titles to sort by them, shift clicking allows multi-sorting</li>
-      |<li>Click on a character / stash tab button to update that stash tab from the server, and to show only it's contents.</li>
-      |<li>Use the clear and reload everything button when you need to update several stash tabs, or you re-ordered them</li>
-      |<li>Sockets in the socket column are displayed first by largest group, then in alphabetical order. That means G-G-R-R won't ever be listed as G-R-G-R or other permutations.</li>
-      |<li>Entering .-.-.-. in the sockets column will match all four link+ socketed items</li>
-      |<li>Entering B-B-G-R in the sockets column will match all four link+ socketed items, that have 2 blues linked with a green and red. </li>
-      |<li>The UI is dark so that you can leave it up on a second monitor while you play.</li>
-      |<li>The score field was roughly based on <a href="http://www.reddit.com/r/pathofexile/comments/1q5rdi/its_worth_keeping_if/">this</a> reddit post</li>
-      |<li>Looty was written in <a href="http://www.scala-js.org/">ScalaJs</a></li>
-      |</ul>
-      |<br>
-      |<br>
-      |<div>
-      |<h3> Version History</h3><br>
-      |<h4>0.2.1.10</h4>
-      | Rewrote Gem XP Tracker. Try it out and let me know what you guys think!<br>
-      | Using select2 menus on the datagrids for views (and split the source into multiple files it was getting fat)<br>
-      | Added ACE / vm.js (for custom scores, not yet implemented)<br>
-      |<h4>0.2.1.9</h4>
-      | <ul>
-      | <li>
-      | Numeric columns don't require operators (e.g. < <= etc) anymore. Those operators can still be used however they should filter
-      | as a min or max automatically. For example typing 50 in the pDps column will only show items with a pDps greater than or equal to 50.
-      | while typing it into the rLvl column (RequiredLevel) will show items that require at most level 50.
-      |</li>
-      | <li>Strength and Int Bonuses are now applied to life / mana</li>
-      | <li>increased cast speed, accuracy and max energy shield all have columns</li>
-      |</ul>
-      |<h4>0.2.1.8</h4>
-      | shift click to refresh a tab / character <br>
-      | more descriptive types <br>
-      | larger column headers / smaller fonts <br>
-      |<h4>0.2.1.7</h4>
-      | Show empty cells instead of zeros <br>
-      |<h4>0.2.1.6</h4>
-      | Fixed sizing issue with PoeBuilder Page <br>
-      |<h4>0.2.1.5</h4>
-      | Add / Remove / Save Columns + Filters  <br>
-      | Add / Remove / Refresh Tabs  <br>
-      | PoeBuilder Integration  <br>
-      |<h4>0.2.1.4</h4>
-      | Fixed bug with Block % not parsing correctly <br>
-      |<h4>0.2.1.3</h4>
-      | Fixed bug with max level gems causing parse errors <br>
-      | Upgrade to ScalaJs 0.5.3 <br>
-      |<h4>0.2.1.2</h4>
-      | Fixed google analytics script errors <br>
-      |<h4>0.2.1.1</h4>
-      |<ul>
-      |<li>
-      |Added a misc column that will show: <br>
-      | count of items in stack / mapLevel / gemLevel / Total #Sockets <br>
-      | <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjvnq3h">hih0</a><br>
-      | <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjvfh33">Sirais</a><br>
-      | <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjvo798">tokyotapes</a><br>
-      |</li>
-      |</ul>
-      |<h4>0.2.1</h4>
-      |<ul>
-      |<li>
-      |Fixed DPS Bug <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjveud4">SoulProxy</a><br>
-      |</li>
-      |</ul>
-      |</div>
-      |</div>
-      """.stripMargin
+        s"""<h1 style="color:red">$banner</h1>""" +
+        """<h2><img src="images/coin16.png">Welcome to <span style="color:gold">Looty!</span><img src="images/coin16.png">""" +
+        s"""</h2>Version: ($version)<p>""" +
+        """<span style="color:gold">Looty</span> was created to make it easier to search inventories in Path of Exile.<br>
+        |It provides a grid interface to search for items in different leagues.<br>
+        |The XP view will attempt to predict when a gem will level up.<br>
+        |GGG seems to only refresh items and gem information when a character changes zones,
+        |so keep that in mind if things aren't showing up.<br>
+        |source code available on <a href="https://github.com/benjaminjackman/looty">github</a>.<br>
+        |<h3>Help and more info available <a href="http://blog.jackman.biz/looty">here</a> (eventually)</h3>
+        |<h4> Tips & Tricks & Miscellaneous Details </h4>
+        |<ul>
+        |<li>You can type things to search for into the empty row at the top of the grids.
+        |<ul>
+        |<li>Try starting the search with &gt; or &lt; or =</li>
+        |<li>The search is not case sensitive.</li>
+        |<li>If the first term isn't an operator, then it defaults to regular expression search.</li>
+        |<li>Spaces between words are treated as an 'OR', so only one word has to match to return a result.</li>
+        |<li>If you need to search for a space just use a \s instead.</li>
+        |<li>For example brass maul, will match 'brass maul' and 'spiny maul' and 'brass monkey'</li>
+        |<li>Whereas brass\smaul will match only 'brass maul'.</li>
+        |</ul>
+        |</li>
+        |<li>You can type into multiple columns to filter down to what you want, for example to find boots with a red
+        |socket that can be worn by characters under level 35, enter <i>boots</i> in the <b>type</b> column
+        |and <i>r</i> in the <b>sockets</b> column and <i>&lt;35</i> in the <b>rlvl</b> column.
+        |</li>
+        |<li>Click the column titles to sort by them, shift clicking allows multi-sorting</li>
+        |<li>Click on a character / stash tab button to update that stash tab from the server, and to show only it's contents.</li>
+        |<li>Use the clear and reload everything button when you need to update several stash tabs, or you re-ordered them</li>
+        |<li>Sockets in the socket column are displayed first by largest group, then in alphabetical order. That means G-G-R-R won't ever be listed as G-R-G-R or other permutations.</li>
+        |<li>Entering .-.-.-. in the sockets column will match all four link+ socketed items</li>
+        |<li>Entering B-B-G-R in the sockets column will match all four link+ socketed items, that have 2 blues linked with a green and red. </li>
+        |<li>The UI is dark so that you can leave it up on a second monitor while you play.</li>
+        |<li>The score field was roughly based on <a href="http://www.reddit.com/r/pathofexile/comments/1q5rdi/its_worth_keeping_if/">this</a> reddit post</li>
+        |<li>Looty was written in <a href="http://www.scala-js.org/">ScalaJs</a></li>
+        |</ul>
+        |<br>
+        |<br>
+        |<div>
+        |<h3> Version History</h3><br>
+        |<h4>0.2.1.10</h4>
+        | Rewrote Gem XP Tracker. Try it out and let me know what you guys think!<br>
+        | Using select2 menus on the datagrids for views (and split the source into multiple files it was getting fat)<br>
+        | Added ACE / vm.js (for custom scores, not yet implemented)<br>
+        |<h4>0.2.1.9</h4>
+        | <ul>
+        | <li>
+        | Numeric columns don't require operators (e.g. < <= etc) anymore. Those operators can still be used however they should filter
+        | as a min or max automatically. For example typing 50 in the pDps column will only show items with a pDps greater than or equal to 50.
+        | while typing it into the rLvl column (RequiredLevel) will show items that require at most level 50.
+        |</li>
+        | <li>Strength and Int Bonuses are now applied to life / mana</li>
+        | <li>increased cast speed, accuracy and max energy shield all have columns</li>
+        |</ul>
+        |<h4>0.2.1.8</h4>
+        | shift click to refresh a tab / character <br>
+        | more descriptive types <br>
+        | larger column headers / smaller fonts <br>
+        |<h4>0.2.1.7</h4>
+        | Show empty cells instead of zeros <br>
+        |<h4>0.2.1.6</h4>
+        | Fixed sizing issue with PoeBuilder Page <br>
+        |<h4>0.2.1.5</h4>
+        | Add / Remove / Save Columns + Filters  <br>
+        | Add / Remove / Refresh Tabs  <br>
+        | PoeBuilder Integration  <br>
+        |<h4>0.2.1.4</h4>
+        | Fixed bug with Block % not parsing correctly <br>
+        |<h4>0.2.1.3</h4>
+        | Fixed bug with max level gems causing parse errors <br>
+        | Upgrade to ScalaJs 0.5.3 <br>
+        |<h4>0.2.1.2</h4>
+        | Fixed google analytics script errors <br>
+        |<h4>0.2.1.1</h4>
+        |<ul>
+        |<li>
+        |Added a misc column that will show: <br>
+        | count of items in stack / mapLevel / gemLevel / Total #Sockets <br>
+        | <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjvnq3h">hih0</a><br>
+        | <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjvfh33">Sirais</a><br>
+        | <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjvo798">tokyotapes</a><br>
+        |</li>
+        |</ul>
+        |<h4>0.2.1</h4>
+        |<ul>
+        |<li>
+        |Fixed DPS Bug <a href="http://www.reddit.com/r/pathofexile/comments/2e22kc/tools_looty_020_now_available_in_the_chrome_web/cjveud4">SoulProxy</a><br>
+        |</li>
+        |</ul>
+        |</div>
+        |</div>
+        """.stripMargin
     }
   }
 
