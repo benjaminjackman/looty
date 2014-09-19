6cef2d2c9f24ebe7ebe521fa2a69a3a3bbdd8720
Ben Jackman <ben@jackman.biz>
2014-09-19 01:27:55 -0500

    Select League, then Character in that league!
    Signed-off-by: Ben Jackman <ben@jackman.biz>

--- looty/src/main/scala/looty/views/GlobalView.scala
+++ looty/src/main/scala/looty/views/GlobalView.scala
@@ -2,19 +2,16 @@ package looty
 package views
 
 import cgta.cenum.CEnum
-
-import japgolly.scalajs.react.{Ref, BackendScope, React, ReactComponentB}
+import japgolly.scalajs.react.{BackendScope, React, ReactComponentB}
 import looty.poeapi.PoeCacher
 import looty.views.GlobalView.GlobalViewRoot
-import looty.views.GlobalView.Leagues.League
-import looty.views.snippets.Select2Wrapper
 import looty.views.widgets.Select2Widget
 import org.scalajs.dom
-import org.scalajs.dom.HTMLDivElement
 import org.scalajs.jquery.JQuery
+import poeapi.PoeTypes.CharacterInfo
+import views.SelectLeagueWidget.Leagues.League
 
 import scala.concurrent.Future
-import scala.scalajs.js
 
 
 //////////////////////////////////////////////////////////////
@@ -26,8 +23,7 @@ import scala.scalajs.js
 //////////////////////////////////////////////////////////////
 
 
-object GlobalView {
-
+object SelectLeagueWidget {
   object Leagues extends CEnum {
     final type EET = League
     sealed trait League extends EnumElement
@@ -39,16 +35,13 @@ object GlobalView {
     final override val elements = CEnum.getElements(this)
   }
 
-  import japgolly.scalajs.react.vdom.ReactVDom._
-  import japgolly.scalajs.react.vdom.ReactVDom.all._
-
-  case class SelectLeagueProps(
+  case class Props(
     initialLeague: Option[League],
     onLeagueChanged: League => Unit)
 
-  val SelectLeagueComponent = {
-    ReactComponentB[SelectLeagueProps]("SelectLeague")
-      .render((props, S) => Select2Widget.component(Select2Widget.Props(
+  val component = {
+    ReactComponentB[Props]("SelectLeague")
+      .render((props) => Select2Widget.component(Select2Widget.Props(
       width = 120,
       placeholder = "League",
       onFilter = (txt) => Future(Leagues.elements.map(_.toString).filter(l => l.toLowerCase.startsWith(txt.toLowerCase))),
@@ -56,18 +49,53 @@ object GlobalView {
     )))
       .create
   }
+}
 
-  val SelectCharacterComponent = {
-    
-  }
+
+object SelectCharacterWidget {
+  case class Props(getCharacters: () => Future[Seq[CharacterInfo]], onCharacterChanged: String => Unit)
+  val component = ReactComponentB[Props]("SelectCharacter")
+    .render((props) => Select2Widget.component(Select2Widget.Props(
+    width = 120,
+    placeholder = "Character",
+    onFilter = (txt) => props.getCharacters().map(cs => cs.map(c => c.name).filter(_.toLowerCase.contains(txt.toLowerCase))),
+    onChange = (txt) => props.onCharacterChanged(txt)
+  ))
+    )
+    .create
+}
+
+
+object GlobalView {
+
 
   class GlobalViewRoot(pc: PoeCacher) {
 
+    import japgolly.scalajs.react.vdom.ReactVDom._
+    import japgolly.scalajs.react.vdom.ReactVDom.all._
+
+    case class State(league: Option[League], character: Option[String])
+    case class Backend(T: BackendScope[_, State]) {
+      def setLeague(league: League) {
+        T.modState(_.copy(league = Some(league), character = None))
+      }
+      def setCharacter(character: String) {
+        T.modState(_.copy(character = Some(character)))
+      }
+      def getCharacters(): Future[Seq[CharacterInfo]] = {
+        pc.getChars().map(cs => cs.toList.filter(c => Option(c.league) =?= T.state.league.map(_.toString)))
+      }
+    }
+
     val component = ReactComponentB[Unit]("GlobalViewRoot")
-      .render((_) =>
+      .initialState(State(None, None))
+      .backend(Backend)
+      .render { (p, s, b) =>
       div(
-        SelectLeagueComponent(SelectLeagueProps(None, (l) => console.log("NEW LEAGUE", l.toString)))
-      ))
+        SelectLeagueWidget.component(SelectLeagueWidget.Props(s.league, b.setLeague)),
+        s.league.map(l => SelectCharacterWidget.component(SelectCharacterWidget.Props(() => b.getCharacters(), (c) => b.setCharacter(c))))
+      )
+    }
       .createU
   }
 
