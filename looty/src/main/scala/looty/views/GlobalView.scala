6042154451f02af05291a995d9c3db532a21c8d2
Ben Jackman <ben@jackman.biz>
2014-09-18 21:34:31 -0500

    Woohoo Got Select2 Working with React!
    Signed-off-by: Ben Jackman <ben@jackman.biz>

---
+++ looty/src/main/scala/looty/views/GlobalView.scala
@@ -0,0 +1,99 @@
+package looty
+package views
+
+import cgta.cenum.CEnum
+
+import japgolly.scalajs.react.{Ref, BackendScope, React, ReactComponentB}
+import looty.poeapi.PoeCacher
+import looty.views.GlobalView.GlobalViewRoot
+import looty.views.GlobalView.Leagues.League
+import looty.views.snippets.Select2Wrapper
+import org.scalajs.dom
+import org.scalajs.dom.HTMLDivElement
+import org.scalajs.jquery.JQuery
+
+import scala.concurrent.Future
+import scala.scalajs.js
+
+
+//////////////////////////////////////////////////////////////
+// Copyright (c) 2014 Ben Jackman, Jeff Gomberg
+// All Rights Reserved
+// please contact ben@jackman.biz or jeff@cgtanalytics.com
+// for licensing inquiries
+// Created by bjackman @ 9/18/14 8:20 PM
+//////////////////////////////////////////////////////////////
+
+
+object GlobalView {
+
+  object Leagues extends CEnum {
+    final type EET = League
+    sealed trait League extends EnumElement
+    case object Standard extends League
+    case object Hardcore extends League
+    case object Rampage extends League
+    case object Beyond extends League
+    //Needs to be overrided in child class with
+    final override val elements = CEnum.getElements(this)
+  }
+
+  import japgolly.scalajs.react.vdom.ReactVDom._
+  import japgolly.scalajs.react.vdom.ReactVDom.all._
+
+  case class SelectLeagueProps(
+    initialLeague: Option[League],
+    onLeagueChanged: League => Unit)
+
+  val SelectLeagueComponent = {
+    ReactComponentB[SelectLeagueProps]("SelectLeague")
+      .getInitialState((p) => p.initialLeague)
+      .render((_, S) => div(ref := "divEl"))
+      .componentDidMount { scope =>
+      val ref = Ref[HTMLDivElement]("divEl")(scope).get
+      val el = ref.getDOMNode()
+      Select2Wrapper(
+        el,
+        120,
+        "League",
+        (txt) => Future(Leagues.elements.map(_.toString).filter(l => l.toLowerCase.startsWith(txt.toLowerCase))))(
+          (txt) => scope.props.onLeagueChanged(Leagues.fromString(txt)))
+    }
+      .create
+  }
+
+  
+
+
+  class GlobalViewRoot(pc: PoeCacher) {
+
+    val component = ReactComponentB[Unit]("GlobalViewRoot")
+      .render((_) =>
+      div(
+        SelectLeagueComponent(SelectLeagueProps(None,(l) => console.log("NEW LEAGUE", l.toString)))
+      ))
+      .createU
+  }
+
+}
+
+class GlobalView(implicit val pc: PoeCacher) extends View {
+
+  case class GlobalBackend(implicit val pc: PoeCacher)
+
+  class Backend(T: BackendScope[_, Vector[String]]) {
+    def handleAdd() {
+    }
+  }
+
+
+  override def start(jq: JQuery): Unit = {
+    val el = jq.get(0).asInstanceOf[dom.Element]
+    val root = new GlobalViewRoot(pc).component()
+    React.renderComponent(root, el)
+  }
+
+  override def stop(): Unit = {
+
+  }
+}
