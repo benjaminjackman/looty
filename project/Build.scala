602ba9bcdd897ed2aa1c4d678f8379ffa7761ecc
Ben Jackman <ben@jackman.biz>
2014-09-20 14:23:19 -0500

    SBT now uses the SbtWeb format
    Signed-off-by: Ben Jackman <ben@jackman.biz>

--- project/Build.scala
+++ project/Build.scala
@@ -1,19 +1,66 @@
+import com.typesafe.sbt.less.Import.LessKeys
+import com.typesafe.sbt.web
+import com.typesafe.sbt.web.SbtWeb
 import org.sbtidea.SbtIdeaPlugin
 import sbt._
 import Keys._
 import scala.scalajs.sbtplugin.ScalaJSPlugin._
-import ScalaJSKeys._
 
 object Build extends sbt.Build {
+  import web.Import._
 
-  val generateHtml = Def.task {
-    val log = streams.value.log
+
+  val handleLootyHtml = Def.task {
+    val css = """
+  <link rel="stylesheet" href="jslib/slickgrid/css/smoothness/jquery-ui-1.8.16.custom.css"/>
+  <link rel="stylesheet" href="jslib/slickgrid/slick.grid.css"/>
+  <link rel="stylesheet" href="jslib/font-awesome-4.2.0/css/font-awesome.css"/>
+  <link rel="stylesheet" href="jslib/select2/select2.css">
+    """
+
+    val jslib = """
+<script type="text/javascript" src="jslib/d3.js"></script>
+<script type="text/javascript" src="jslib/jquery.js"></script>
+<script type="text/javascript" src="jslib/jquery-migrate.js"></script>
+<script type="text/javascript" src="jslib/react-with-addons.js"></script>
+<script type="text/javascript" src="jslib/signals.js"></script>
+<script type="text/javascript" src="jslib/crossroads.js"></script>
+<script type="text/javascript" src="jslib/hasher.js"></script>
+<script type="text/javascript" src="jslib/mespeak/mespeak.full.js"></script>
+<script type="text/javascript" src="jslib/esprima.js"></script>
+<script type="text/javascript" src="jslib/vm.js"></script>
+<script type="text/javascript" src="jslib/ace/ace.js" charset="utf-8"></script>
+<script type="text/javascript" src="jslib/select2/select2.js"></script>
+<script type="text/javascript" src="jslib/filesaver.js"></script>
+<script type="text/javascript" src="jslib/slickgrid/lib/jquery.event.drag-2.0.min.js"></script>
+<script type="text/javascript" src="jslib/slickgrid/lib/jquery.event.drop-2.0.min.js"></script>
+<script type="text/javascript" src="jslib/slickgrid/slick.core.js"></script>
+<script type="text/javascript" src="jslib/slickgrid/slick.grid.js"></script>
+<script type="text/javascript" src="jslib/slickgrid/slick.dataview.js"></script>
+    """
+
+    val analytics  = """
+<script type="text/javascript" src="jslib/analytics.js"></script>
+<script src="https://ssl.google-analytics.com/ga.js" async="true"></script>
+<script type="text/javascript" src="jslib/startup.js"></script>
+    """
 
     //Copy over the html files, while filling in the template sections
-    val htmlSrcDir: File = (sourceDirectory in Compile).value / "html"
+    val lessFiles = {
+      val i = (includeFilter in (Assets, LessKeys.less)).value
+      val x = (excludeFilter in (Assets, LessKeys.less)).value
+      val srcDir = (sourceDirectory in Assets).value
+      val lessSrcs = (sourceDirectory in Assets).value ** (i -- x)
+      val f = lessSrcs.pair(relativeTo(srcDir))
+      f.map(_._2).map(_.replace(".less", ".css"))
+    }
+
+    val less = lessFiles.map(f=>s"""<link rel="stylesheet" href="$f"/>""").mkString("\n")
+
+    val htmlSrcDir: File = (sourceDirectory in Assets).value
     val htmlSrcs: PathFinder = htmlSrcDir * "*.template.html"
-    val outDir: File = (crossTarget in Compile).value
-    val outMappings = htmlSrcs x Path.rebase(htmlSrcDir, outDir)
+    val outDir: File = WebKeys.webTarget.value / "public" / "main"
+    val outMappings = htmlSrcs pair Path.rebase(htmlSrcDir, outDir)
     outMappings.flatMap {
       case (in, out0) =>
         val content = IO.read(in)
@@ -27,40 +74,38 @@ object Build extends sbt.Build {
         val devScripts = """<script type="text/javascript" src="looty-fastopt.js"></script>""".stripMargin
         val releaseScripts = """<script type="text/javascript" src="looty-opt.js"></script>"""
 
-        val devOut = content.replace("<!-- insert scalajs -->", devScripts)
-        val releaseOut = content.replace("<!-- insert scalajs -->", releaseScripts)
+        def addAll(sjsScripts : String) : String = {
+          var res = content
+          res = res.replace("<!-- insert scalajs -->", sjsScripts)
+          res = res.replace("<!-- insert css -->", css)
+          res = res.replace("<!-- insert less -->", less)
+          res = res.replace("<!-- insert jslib -->", jslib)
+          res = res.replace("<!-- insert analytics -->", analytics)
+          res
+        }
+
+        val devOut = addAll(sjsScripts = devScripts)
+        val releaseOut = addAll(sjsScripts = releaseScripts)
 
         IO.write(devFile, devOut.getBytes("UTF-8"))
         IO.write(releaseFile, releaseOut.getBytes("UTF-8"))
         List(devFile, releaseFile)
-
     }
 
     Seq[File](outMappings.map(_._1): _*)
   }
 
-  val copyAll = Def.task {
-    val outDir = (crossTarget in Compile).value
 
-    IO.copyFile((resourceDirectory in Compile).value / "manifest.json", outDir / "manifest.json")
-    IO.copyFile((sourceDirectory in Compile).value / "html" / "popup.html", outDir / "popup.html")
-    IO.copyDirectory((resourceDirectory in Compile).value / "images", outDir / "images")
-    IO.copyDirectory((resourceDirectory in Compile).value / "data", outDir / "data")
-    IO.copyDirectory((baseDirectory in Compile).value / "jslib", outDir / "jslib")
+  lazy val sjsTasks = List(
+    ScalaJSKeys.packageExternalDepsJS,
+    ScalaJSKeys.packageInternalDepsJS,
+    ScalaJSKeys.packageExportedProductsJS,
+    ScalaJSKeys.fastOptJS,
+    ScalaJSKeys.fullOptJS)
 
-    val out = Seq[File](
-      (resourceDirectory in Compile).value / "manifest.json",
-      (sourceDirectory in Compile).value / "html" / "popup.html",
-      (resourceDirectory in Compile).value / "images",
-      (resourceDirectory in Compile).value / "data",
-      (baseDirectory in Compile).value / "jslib"
-    )
+  lazy val sjsOutDir = Def.settingKey[File]("directory for javascript files output by scalajs")
 
-    out
-  }
-
-
-  def pject(name: String) = Project(name, file(name)).settings(
+  lazy val looty: Project = Project("looty", file("looty")).settings(
     scalaJSSettings: _*).settings(
       scalacOptions += "-deprecation",
       scalacOptions += "-unchecked",
@@ -83,17 +128,21 @@ object Build extends sbt.Build {
         "biz.cgta" %%% "serland-sjs" % "0.1.0"
       )
     )
+    .settings(
+      includeFilter in (Assets, LessKeys.less) := "*.less",
+      excludeFilter in (Assets, LessKeys.less) := "_*.less"
+    )
+    .settings(
+      SbtIdeaPlugin.ideaBasePackage := Some("looty"),
+      sourceGenerators in Assets <+= handleLootyHtml,
+      libraryDependencies += "org.scala-lang.modules.scalajs" %%% "scalajs-jquery" % "0.6",
+      libraryDependencies += "com.github.japgolly.scalajs-react" %%% "core" % "0.4.1")
+    .settings((ScalaJSKeys.fastOptJS in Compile) <<= (ScalaJSKeys.fastOptJS in Compile).dependsOn(WebKeys.assets in Assets))
+    .settings(sjsOutDir := WebKeys.webTarget.value / "public" / "main" )
+    .settings(sjsTasks.map(t => crossTarget in(Compile, t) := sjsOutDir.value): _*)
+    .enablePlugins(SbtWeb)
 
-  lazy val looty = pject("looty").settings(
-    SbtIdeaPlugin.ideaBasePackage := Some("looty"),
-    resourceGenerators in Compile <+= generateHtml,
-    watchSources += (sourceDirectory in Compile).value / "html",
-    resourceGenerators in Compile <+= copyAll,
-    libraryDependencies += "org.scala-lang.modules.scalajs" %%% "scalajs-jquery" % "0.6",
-    libraryDependencies += "com.github.japgolly.scalajs-react" %%% "core" % "0.4.1"
-  )
-
-  lazy val root = Project("root", file(".")).aggregate(looty)
+  lazy val root = Project("root", file(".")).aggregate(looty).enablePlugins(SbtWeb)
 
   object Libs {
   }
--- project/build.sbt
+++ project/build.sbt
@@ -1,5 +1,7 @@
-resolvers += Resolver.url("scala-js-releases", url("http://repo.scala-js.org/repo/releases/"))(Resolver.ivyStylePatterns)
+scalacOptions += "-deprecation"
 
 addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.5.4")
 
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-less" % "1.0.2")
