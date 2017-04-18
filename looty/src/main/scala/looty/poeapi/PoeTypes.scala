267b11dd9281ce9c6b8d6f92de6a178238f946c4
Peter Lukasiewicz <k_iller21@hotmail.com>
2017-04-18 23:18:41 +0200

    Improves handling for map fragments
    As long as GGG keeps using the same description text pattern for
    fragments, this will be fine

--- looty/src/main/scala/looty/poeapi/PoeTypes.scala
+++ looty/src/main/scala/looty/poeapi/PoeTypes.scala
@@ -145,7 +145,7 @@ object PoeTypes {
       val all = Array(normal, magic, rare, unique, gem, currency, divCard, quest, prophecy, relic)
     }
 
-    val mapFragments = ISet(
+    /*val mapFragments = ISet(
       "Sacrifice at Dusk",
       "Sacrifice at Midnight",
       "Sacrifice at Dawn",
@@ -164,10 +164,7 @@ object PoeTypes {
       "Fragment of the Phoenix",
       "Fragment of the Hydra",
       "Ancient Reliquary Key")
-
-    // TODO - find out if breach splinters and breachstones are currency, fragments or both?
-    // add breach splinters
-    // add breach fragments
+    */
 
     implicit class AnyItemExtensions(val x: AnyItem) extends AnyVal {
       def isGem = x.getFrameType == FrameTypes.gem
@@ -177,7 +174,8 @@ object PoeTypes {
       def isCurrency = x.getFrameType == FrameTypes.currency && !isHideoutItem
       def isQuest = x.getFrameType == FrameTypes.quest
       def isMap = x.descrText.toOption.exists(_ contains "Travel to this Map")
-      def isFragment = mapFragments(x.typeLine)
+      // all map fragments that can be used in the map device contain this bit of text in their description
+      def isFragment = x.descrText.toOption.exists(_ contains "Eternal Laboratory or a personal")
       def isFlask = x.descrText.toOption.exists(_ contains "Right click to drink.")
       def isJewel = x.descrText.toOption.exists(_ contains "Jewel Socket")
       def isDivinationCard = x.getFrameType == FrameTypes.divCard
