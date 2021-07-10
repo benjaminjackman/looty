9b5d10edcf0538e36ef1b35c88655ad2dbf6ba5e
Piotr Szymaniec <traf@angrenost.org>
2021-07-10 19:35:21 +0200

    Increment Version number to 0.2.1.82

--- looty/src/main/scala/looty/views/HomeView.scala
+++ looty/src/main/scala/looty/views/HomeView.scala
@@ -13,6 +13,14 @@ class HomeView(val banner: String, val version: String) extends View {
 	val jq: JQueryStatic = global.jQuery.asInstanceOf[JQueryStatic]
   def versionHistory = """
 # Version History
+## 0.2.1.82 (2021-07-10)
+Fixes
+Duplicate requests problem solved -> faster item data download
+
+Dev
+Added boolean variable to turn off console parsing messages.
+By default they are shown, as before.
+
 ## 0.2.1.81 (2021-04-25) (Traf contributed this update)
 
 Features
